7841fa725edd027ff5ad1aecde1e90a9584bb49b
mpyat2 <44919945+mpyat2@users.noreply.github.com>
2018-11-29 09:26:29 +0200

    Version 0.05
    Version 0.05: launching script via Rscript

--- run.bat
+++ run.bat
@@ -5,28 +5,25 @@ REM === BEGIN OF CONFIGURATION SECTION
 REM Set path to your R interpreter
 SET RPATH="C:\Program Files\R\R-3.5.1\bin\x64\"
 
+REM Set script name
+SET SCRIPT=plot_model2.R
+
 REM === END OF CONFIGURATION SECTION
 
 REM Startup options
-SET ROPTIONS=--no-save --no-environ --no-site-file --no-restore --quiet
+SET ROPTIONS=--vanilla --quiet
 
 REM CD to BATCH file path
 CD /D %~dp0
 
 ECHO Current directory: %CD%
 
-IF NOT EXIST plot_model2.R GOTO :ERROR2
+IF NOT EXIST %SCRIPT% GOTO :ERROR_SCRIPT_NOT_FOUND
 
-IF NOT EXIST .Rprofile ECHO .First = function() { source("plot_model2.R") } > .Rprofile
-IF NOT EXIST .Rprofile GOTO :ERROR1
-%RPATH%\R %ROPTIONS%
+%RPATH%\Rscript %ROPTIONS% -e "source('%SCRIPT%')"
 
 GOTO :EOF
 
-:ERROR1
-ECHO **** ERROR: Cannot create .Rprofile
-PAUSE
-
-:ERROR2
-ECHO **** ERROR: Cannot find script: plot_model2.R
+:ERROR_SCRIPT_NOT_FOUND
+ECHO **** ERROR: Cannot find script: %SCRIPT%
 PAUSE
